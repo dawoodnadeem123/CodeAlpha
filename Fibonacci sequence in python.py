860bebc5ac3efed87754021c9f50b9c3231f313c
dawoodnadeem123 <131924433+dawoodnadeem123@users.noreply.github.com>
2023-11-10 21:17:37 +0500

    Add files via upload

---
+++ Fibonacci sequence in python.py
@@ -0,0 +1,29 @@
+#Program to display the Fibonacci sequence up to n-th term
+for dawood in range(0,30,1):
+    nterms = int(input("How many terms? "))
+
+    # first two terms
+    n1, n2 = 0, 1
+    count = 0
+
+    # check if the number of terms is valid
+    if nterms <= 0:
+        print("Please enter a positive integer")
+   # if there is only one term, return n1
+    elif nterms == 1:
+        print("Fibonacci sequence upto",nterms,":")
+        print(n1)
+   #generate fibonacci sequence
+    else:
+       print("Fibonacci sequence:")
+       while count < nterms:
+           print(n1)
+           nth = n1 + n2
+           # update values
+           n1 = n2
+           n2 = nth
+           count += 1
+    ANS=input("do you want to still use fibonaci sequence generator ",)
+    if ANS=='no':
+        break
+    
